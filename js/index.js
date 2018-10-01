ede78179c4efc2bd2eb9d8dde0ac63d7a26effaf
HadiHilal <hadi-w95@hotmail.com>
2018-10-02 00:53:39 +0300

    responsive

---
+++ js/index.js
@@ -0,0 +1,20 @@
+// glopal
+$(document).ready(function(){
+    'use strict';
+	jQuery('.skillbar').each(function(){
+		jQuery(this).find('.skillbar-bar').animate({
+			width:jQuery(this).attr('data-percent')
+		},6000);
+	});
+});
+
+jQuery('.Count').each(function () {
+  var $this = $(this);
+  jQuery({ Counter: 0 }).animate({ Counter: $this.text() }, {
+    duration: 6000,
+    easing: 'swing',
+    step: function () {
+      $this.text(Math.ceil(this.Counter));
+    }
+  });
+});
